d634662c07217fa932ffa10e6a437583d38a5bff
Brian Graf <brian.g.graf@gmail.com>
2020-01-03 13:27:26 -0700

    Added local/session storage logins and added logic to change pages based on that data

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
@@ -11,15 +11,22 @@ import About from "./components/pages/About";
 
 import API from "./utils/API";
 import NavTabs from './components/NavTabs';
+// import { ConnectionBase } from 'mongoose';
 
 function App() {
 
-  const isLoggedIn = checkIfLoggedIn();
+  const [isLoggedIn, setIsLoggedIn] = useState([]);
 
   const [players, setPlayers] = useState([]);
 
+  const [login, setLogin] = useState([]);
+
   useEffect(() => {
-    // DB_getPlayers();
+    setLogin({
+      name: "login"
+    });
+
+    setIsLoggedIn(checkIfLoggedIn());
   }, []);
 
 
@@ -30,12 +37,18 @@ function App() {
       <NavTabs />
       <Route exact path="/">
         {/* Redirect to login if the user is not logged in.  Else, redirect home */}
-        {isLoggedIn ? <Redirect to="/home"/> : <Redirect to="/login"/>}
+        {isLoggedIn ? <Redirect to="/home" /> : <Redirect to="/login" />}
       </Route>
-      <Route exact path="/login" render={(props) => <Login {...props} />} />
+
+    <Route exact path = "/login">
+      {isLoggedIn ? <Redirect to = "/home" /> : <Redirect to="/login"></Redirect>}
+
+    </Route>
+
+      <Route exact path="/login" render={(props) => <Login {...props} handleInputChange={handleLoginInputChange} handleSubmit={handleLoginSubmit}/>} />
       <Route exact path="/home" render={(props) => <Home {...props} />} />
       <Route exact path="/play" render={(props) => <Play {...props} />} />
-      <Route exact path="/data" render={(props) => <Data {...props} players={players} displayPlayers={displayPlayers} DB_getPlayers={DB_getPlayers}/>} />
+      <Route exact path="/data" render={(props) => <Data {...props} players={players} displayPlayers={displayPlayers} DB_getPlayers={DB_getPlayers} />} />
       <Route exact path="/about" render={(props) => <About {...props} />} />
     </Router>
 
@@ -72,10 +85,68 @@ function App() {
    * Checks if the user is logged in
    * @returns true if the user is logged in, false if not.
    */
-  function checkIfLoggedIn(){
-    return false;
+  function checkIfLoggedIn() {
+    const retrievedStorage = sessionStorage.getItem("login");
+    console.log("Getting session login..." + retrievedStorage);
+
+    //If we find data with the name 'login', then the user is logged in.
+    if (retrievedStorage) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
+  /**
+   * Updates the state of the login based on the input name and value
+   * @param {Object} event - If an object, the change event fired when the input value is changed.
+   * @param {String} event - If a string, the value to be set to the email.
+   */
+  function handleLoginInputChange(event) {
+
+    const { target } = event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+
+    setLogin({ ...login, [name]: value });
+
+    console.log(login);
+  }
+
+  /**
+   * This event handles login submits, sending the login information to the session storage.
+   * @param {Object} event - The submit event fired.
+   */
+  function handleLoginSubmit(event) {
+    event.preventDefault();
+
+    if (checkAutho()) {
+      console.log("storing to session storage...");
+      console.log(login);
+
+      const login_lessPassword = login;
+      delete login_lessPassword["password"];
+      delete login_lessPassword["password-confirm"];
+
+      //We do not store the password, only the email.
+      sessionStorage.setItem(login.name, JSON.stringify(login_lessPassword));
+      localStorage.setItem(login.name, JSON.stringify(login_lessPassword));
+
+    }
+    else {
+      console.log("User not authorized...");
+      alert("You are not authorized");
+    }
+
+  }
+
+  /**
+   * @returns true if the user login is authorized.
+   */
+  function checkAutho() {
+    return true;
+  }
 }
 
 export default App;
--- web-client/src/components/pages/Login.js
+++ web-client/src/components/pages/Login.js
@@ -1,51 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Login(props) {
 
-  const isRegistered = checkIfRegistered();      //A boolean value whether the user has registered or not.
+  const [isRegistered, setIsRegistered] = useState([]);      //A boolean value whether the user has registered or not.
+
+  useEffect(() => {
+    setIsRegistered(checkIfRegistered());
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
 
-        <h1>Login</h1>
-        <h3>Is the player registered already?</h3>
-        <h3>Login data should be cached in the browser</h3>
-        <h5>For this I can use a service worker... I'll need to review that</h5>
+        <h1>Register</h1>
+        {isRegistered ? <h4>Welcome back, please log in</h4> : <h4>You're not registered in our systems</h4>}
 
-        {isRegistered ? 
-        <div>
-          <h1>Username</h1>
-          <input></input>
-          <h1>Password</h1>
-          <input></input>
-        </div>
-        :
-        <div>
-            <h1>Email</h1>
-            <input></input>
-          <h1>Username</h1>
-          <input></input>
-          <h1>Password</h1>
-          <input type = "password"></input>
-          <h1>Repeat Password</h1>
-          <input type = "password"></input>
-        </div>
-      }
+        {isRegistered ?
+          <div>
+            <form>
+
+              <div className="form-group">
+                <label htmlFor="email">Email Address</label>
+                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email" onChange={props.handleInputChange}></input>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" className="form-control" id="password" placeholder="Password" name="password" onChange={props.handleInputChange}></input>
+              </div>
+
+              <button type="submit" value="Submit" onClick={props.handleSubmit}>Submit</button>
+
+            </form>
+          </div>
+          :
+          <div>
+            <form>
+
+
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input type="username" className="form-control" id="username" aria-describedby="usernameHelp" placeholder="Enter username" name="username" onChange={props.handleInputChange}></input>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="email">Email Address</label>
+                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email" onChange={props.handleInputChange}></input>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" className="form-control" id="password" placeholder="Password" name="password" onChange={props.handleInputChange}></input>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password-confirm">Repeat Password</label>
+                <input type="password" className="form-control" id="password-confirm" placeholder="Confirm Password" name="password-confirm" onChange={props.handleInputChange}></input>
+              </div>
+
+              <button type="submit" value="Submit" onClick={props.handleSubmit}>Submit</button>
+
+            </form>
+          </div>
+        }
 
       </header>
 
     </div>
 
   )
+
+  /**
+   * Checks if the user is logged in by checking the local storage.
+   * @returns true if the user is logged in, false if not.
+   */
+  function checkIfRegistered() {
+    console.log("checking for registered user...");
+
+    const localLogin = localStorage.getItem("login");
+
+    if (localLogin) {
+      const login_JSON = JSON.parse(localLogin);
+      console.log(`Found a login with the email of : ${login_JSON.email}`);
+      return true;
+    }
+    else {
+      console.log(`Could not find login stored in local storage`);
+      return false;
+    }
+
+  }
+
 }
 
-/**
- * Checks if the user is logged in by checking the local storage.
- * @returns true if the user is logged in, false if not.
- */
-function checkIfRegistered() {
-  return false;
-}
 
 export default Login;
