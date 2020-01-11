77b8c6bcf82c0dcadb8983261ac5ad658de6190e
Brian Graf <Brian.g.graf@gmail.com>
2020-01-11 10:56:47 -0700

    Built for Heroku

---
+++ client/build/precache-manifest.ba57af9f863678f6871fb3b1e5a67891.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8624d406e99546aadfa835d0e5594cbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95b0277bac688c02a2d3",
+    "url": "/static/css/main.70fb0e94.chunk.css"
+  },
+  {
+    "revision": "52deab7f9255f855195e",
+    "url": "/static/js/2.446c6d9d.chunk.js"
+  },
+  {
+    "revision": "9aef78f5eb38fa8169c3565735dc6030",
+    "url": "/static/js/2.446c6d9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95b0277bac688c02a2d3",
+    "url": "/static/js/main.909c3b01.chunk.js"
+  },
+  {
+    "revision": "fbaf048f868100b869bd",
+    "url": "/static/js/runtime-main.8f658356.js"
+  },
+  {
+    "revision": "aab5a6d134532dd67f83f59779282dc8",
+    "url": "/static/media/web-bg.aab5a6d1.png"
+  }
+]);
